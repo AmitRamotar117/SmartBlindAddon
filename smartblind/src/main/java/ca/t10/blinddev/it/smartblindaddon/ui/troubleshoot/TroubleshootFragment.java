e36323c9db8d02d0dfe6270a5c1045bf185504df
amitpunit <amitpunit117@gmail.com>
2022-06-11 17:23:02 -0400

    added image to TroubleshootFragment

--- smartblind/src/main/java/ca/t10/blinddev/it/smartblindaddon/ui/troubleshoot/TroubleshootFragment.java
+++ smartblind/src/main/java/ca/t10/blinddev/it/smartblindaddon/ui/troubleshoot/TroubleshootFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import ca.t10.blinddev.it.smartblindaddon.R;
 import ca.t10.blinddev.it.smartblindaddon.databinding.FragmentTroubleshootBinding;
 
 public class TroubleshootFragment extends Fragment {
@@ -23,9 +26,13 @@ public class TroubleshootFragment extends Fragment {
 
         binding = FragmentTroubleshootBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        TextView instruct = root.findViewById(R.id.troubleshoot_instruct);
+        ImageView timg = root.findViewById(R.id.troubleshoot_image);
+        instruct.setText("Measure height of blind in cm to calibarate the blind");
+        instruct.setTextSize(15);
+        timg.setImageResource(R.drawable.blinds_mount_measuring_1024x633);
 
-        //final TextView textView = binding.textGallery;
-        //troubleshootViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         return root;
     }
 

2b46923ada90e71ac5e8c12f8ef25f08feac4ea5
amitpunit <amitpunit117@gmail.com>
2022-06-16 19:13:01 -0400

    add new user activity

---
+++ smartblind/src/main/java/ca/t10/blinddev/it/smartblindaddon/NewUserActivity.java
@@ -0,0 +1,14 @@
+package ca.t10.blinddev.it.smartblindaddon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user);
+    }
+}

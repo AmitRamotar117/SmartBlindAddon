3f138d79767825fed043ee04532ca421b732a9c2
amitpunit <amitpunit117@gmail.com>
2022-06-16 20:23:13 -0400

    add new user activity

--- smartblind/src/main/java/ca/t10/blinddev/it/smartblindaddon/NewUserActivity.java
+++ smartblind/src/main/java/ca/t10/blinddev/it/smartblindaddon/NewUserActivity.java
@@ -1,7 +1,9 @@
 package ca.t10.blinddev.it.smartblindaddon;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 public class NewUserActivity extends AppCompatActivity {
@@ -10,5 +12,17 @@ public class NewUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+    }
+
+    @Override
+    public void onBackPressed(){
+        new AlertDialog.Builder(this).setTitle(R.string.app_name)
+                .setMessage(R.string.leave_app).setIcon(R.drawable.ic_exit)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                }).setNegativeButton(R.string.no,null).show();
     }
 }
--- smartblind/src/main/java/ca/t10/blinddev/it/smartblindaddon/SettingsActivity.java
+++ smartblind/src/main/java/ca/t10/blinddev/it/smartblindaddon/SettingsActivity.java
@@ -1,5 +1,6 @@
 package ca.t10.blinddev.it.smartblindaddon;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -30,5 +32,16 @@ public class SettingsActivity extends AppCompatActivity {
         Intent intent = new Intent (SettingsActivity.this, MainActivity.class);
         startActivity(intent);
         finish();
+    }
+    @Override
+    public void onBackPressed(){
+        new AlertDialog.Builder(this).setTitle(R.string.app_name)
+                .setMessage(R.string.leave_app).setIcon(R.drawable.ic_exit)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                }).setNegativeButton(R.string.no,null).show();
     }
 }

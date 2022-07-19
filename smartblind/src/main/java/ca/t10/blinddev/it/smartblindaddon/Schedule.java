af211be56be083eff44f9fd07f7336b09f0b85f5
ChrisMutuc4607 <chrisjanellemutuc@gmail.com>
2022-07-19 14:22:43 -0400

    creating getter and setter for firebase in schedule screen.

---
+++ smartblind/src/main/java/ca/t10/blinddev/it/smartblindaddon/Schedule.java
@@ -0,0 +1,45 @@
+package ca.t10.blinddev.it.smartblindaddon;
+
+public class Schedule {
+    private String  type;
+    private String date;
+    private String time;
+    private String location;
+
+public Schedule(){
+
+}
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
+

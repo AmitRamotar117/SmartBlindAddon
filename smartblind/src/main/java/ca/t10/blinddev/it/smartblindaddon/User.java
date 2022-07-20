3936fd5d78ffe7844672dcc32a8b6bd976b04e41
Andrew <n01309442@humbermail.ca>
2022-07-20 02:33:30 -0400

    -Create user.java to hold the user's information when registering

---
+++ smartblind/src/main/java/ca/t10/blinddev/it/smartblindaddon/User.java
@@ -0,0 +1,15 @@
+package ca.t10.blinddev.it.smartblindaddon;
+
+public class User {
+    public String name, email;
+
+    public User()
+    {
+
+    }
+    public User(String name, String email)
+    {
+        this.name = name;
+        this.email= email;
+    }
+}
